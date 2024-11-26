93f6ff5036f551ee104b578585f7ff5473a0d61b
Nurjahonbek <nurjahonbekweb@gmail.com>
2024-11-26 22:17:29 +0500

    .

---
+++ main.js
@@ -0,0 +1,583 @@
+// 1) Berilgan ikkita sonning har biri musbat ekanligini aniqlang.
+
+// let x1 = +prompt("1-sonni kiriting")
+// let x2 = +prompt("2-sonni kiriting")
+
+// if(x1>0 && x2>0){
+//     console.log(true);
+    
+// }else{
+//     console.log(false);
+    
+// }
+
+// 2)Uchta son berilgan. Ulardan hech bo'lmaganda bittasi manfiy bo'lishi mumkinmi
+
+// let x1 = +prompt("1-sooni kirting ")
+// let x2 = +prompt("2-sooni kirting ")
+// let x3 = +prompt("3-sooni kirting ")
+
+// if(x1 < 0 || x2 < 0 || x3 < 0){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 3)Berilgan son juft va musbatmi?
+
+// let x = +prompt("soni kirting ")
+
+// if(x % 2 ==0 && x>0){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 4)Ikki sonning yig'indisi ularning ko'paytmasidan katta ekanligini tekshiring
+
+// let x1 = +prompt("1-sonni kiriting ")
+// let x2 = +prompt("2-sonni kiriting ")
+
+// if(x1 + x2 > x1 * x2){
+//     console.log(true);
+    
+// }
+// else {
+//     console.log(false);
+    
+// }
+
+// 5)Berilgan son 100 dan katta va 999 dan kichik ekanligini aniqlang.
+
+// let x = +prompt("sonni kirting ")
+
+// if(x>100 && x<999){
+//     console.log(true);
+    
+// }else{
+//     console.log(false);
+    
+// }
+
+// 6)Uchta sonning yig'indisi musbat ekanligini tekshiring.
+
+// let x1 = +prompt("1-sonni kiriting ")
+// let x2 = +prompt("2-sonni kiriting ")
+// let x3 = +prompt("3-sonni kiriting ")
+
+// if(x1 + x2 + x3 > 0){
+//     console.log(true);
+    
+// }else{
+//     console.log(false);
+    
+// }
+
+
+// 7)Berilgan ikkita sonning biri 0 bo'lmasligini tekshiring.
+
+// let x1 = +prompt("1-soni kiriting")
+// let x2 = +prompt("2-soni kiriting")
+
+// if(x1 != 0 || x2 != 0){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 8)Ikki sonning ko'paytmasi musbat va ularning ayirmasi manfiy ekanligini aniqlang.
+
+// let x1 = +prompt("1-sonni kiriting")
+// let x2 = +prompt("2-sonni kiriting")
+
+// if(x1 * x2 > 0 && x1 - x2 <0){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 9)Berilgan son ikki xonali musbat sonmi
+
+// let x = +prompt("sonni kiriting ")
+
+// if(x >= 10 && x<= 99){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 10)Berilgan son juft va 5 ga karrali ekanligini tekshiring
+
+// let x = +prompt("sonni kiriting")
+
+// if(x % 2 ==0 && x % 5 ==0 ){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 11)Uchta sonning hech bo'lmaganda ikkitasi teng ekanligini aniqlang
+
+// let x1 = +prompt("1-sonni kiriting ")
+// let x2 = +prompt("2-sonni kiriting ")
+// let x3 = +prompt("3-sonni kiriting ")
+
+// if(x1 == x2 || x2 == x3 || x3 == x1){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 12)Berilgan son 7 ga bo'linadi va 3 ga bo'linmaydimi?
+
+// let x = +prompt(" sonni kiriting")
+
+// if(x % 7 ==0 && x % 3 != 0){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 13)Ikkita sonning biri 0 yoki ikkalasi ham manfiy ekanligini tekshiring
+
+// let x1 = +prompt("1- sonni kiriting")
+// let x2 = +prompt("2- sonni kiriting")
+
+// if((x1 == 0 || x2 == 0 )||(x1<0 && x2<0 )){
+//     console.log(true);
+    
+// }else{
+//     console.log(false);
+    
+// }
+
+// 14)Uchta sonning eng kattasi musbat ekanligini aniqlang
+
+// let x1 = +prompt('1- sonni kiriting')
+// let x2 = +prompt('2- sonni kiriting')
+// let x3 = +prompt('3- sonni kiriting')
+
+// if((x1 > x2 && x1 > x3 && x2>x1 && x2>x3 && x3> x2 && x3> x1) > 0){ //max funkisiyadan ham foydalansa bolar edi 
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 15)Berilgan son 10 va 20 orasida emasligini aniqlang.
+
+// let x = +prompt('sonni kiriting')
+
+// if(!(x>=10 && x<=20)){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 16)Ikkita sonning yig'indisi manfiy yoki ularning ko'paytmasi musbat ekanligini tekshiring.
+
+// let x1 = +prompt("1-ssonni kiriting")
+// let x2 = +prompt("2-ssonni kiriting")
+
+// if(x1 + x2 < 0 || x1 * x2 > 0 ){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 17)Uchta sonning eng kichigi juft ekanligini aniqlang.
+
+// let x1 = +prompt("1-sonni kiriting")
+// let x2 = +prompt("2-sonni kiriting")
+// let x3 = +prompt("3-sonni kiriting")
+
+// let res = (( Math.min(x1, x2, x3)) % 2 ==0)
+// console.log(res);
+
+// 18)Berilgan sonning oxirgi raqami 5 yoki 0 ekanligini tekshiring
+
+// let x = +prompt('sonnn kiritinng')
+
+// let res = (x % 10 == 5 || x % 10 ==0)
+// console.log(res);
+
+
+// 19)Ikki sonning biri musbat va ikkinchisi manfiy ekanligini aniqlang.
+
+// let x1 = +prompt("1-sonni kiriting")
+// let x2 = +prompt("2-sonni kiriting")
+
+// let res = ((x1 > 0 || x2>0) && (x1<0 || x2<0) )
+// console.log(res);
+
+// 20)Uchta sonning hech bo'lmaganda bittasi juft ekanligini tekshiring
+
+// let x1 = +prompt("1-sonni kiriting")
+// let x2 = +prompt("2-sonni kiriting")
+// let x3 = +prompt("3-sonni kiriting")
+
+// if(x1 % 2==0){
+//     console.log("1-son juft");
+    
+// }
+// if(x2 % 2==0){
+//     console.log("2-son juft");
+    
+// }
+// if(x3 % 2==0){
+//     console.log("3-son juft");
+    
+// }
+
+// 22)Berilgan ikki sonning biri ikkinchisiga karrali ekanligini tekshiring.
+
+// let x1 = +prompt('1-sonni kiriting')
+// let x2 = +prompt('2-sonni kiriting')
+
+// if( x1 ==0 || x2 ==0){
+//     console.log(false);
+    
+// }
+
+// if(x1 % x2 ==0  || x2  % x1 ==0){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 23)Uchta sonning yig'indisi 0 yoki ko'paytmasi manfiy ekanligini aniqlang.
+
+// let x1 = +prompt("1-sonni kiriting ")
+// let x2 = +prompt("2-sonni kiriting ")
+// let x3 = +prompt("3-sonni kiriting ")
+
+// if(x1 + x2 + x3 ==0 || x1 * x2 * x3 <0){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 24)Berilgan son 3 ga va 4 ga bo'linadi, lekin 12 ga bo'linmaydimi?
+
+// let x = +prompt("sonni kiriting ")
+
+// if(x % 3 ==0 && x % 4 ==0 || x % 12 != 0){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 25)Ikki sonning biri toq va ikkinchisi juft ekanligini aniqlang
+
+// let x1 = +prompt("1-soonni kirting")
+// let x2 = +prompt("2-soonni kirting")
+
+// if( (x1 % 2 ==0 && x2 % 2 ==1) || (x2 % 2 ==0 && x1 % 2 ==1)){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 26)Berilgan son musbat va uch xonali emasligini aniqlang
+
+// let x = +prompt("sonni kiriting")
+
+// if(x > 0 && (x < 100 || x > 999)){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 27)Uchta sonning yig'indisi 100 dan katta yoki ko'paytmasi manfiy ekanligini tekshiring.
+
+// let x1 = +prompt("1-sonni kiriting ")
+// let x2 = +prompt("2-sonni kiriting ")
+// let x3 = +prompt("3-sonni kiriting ")
+
+// if(x1 + x2 + x3 > 100 || x1 * x2 * x3 < 0){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 28)Ikki sonning biri 10 dan katta va ikkinchisi 0 dan kichik ekanligini aniqlang.
+
+// let x1 = +prompt("1-sonni kiriting")
+// let x2 = +prompt("2-sonni kiriting")
+
+// if((x1 > 10 && x2 < 0 ) || (x1 < 0 && x2 > 10)){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 29)Berilgan son toq va musbatmi?
+
+// let x = +prompt("sonni kiriting ")
+
+// if(x > 0 && x % 2 == 1){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+ // 30)Ikki sonning hech bo'lmaganda bittasi musbat ekanligini tekshiring 
+
+// let x1 = +prompt("1-sonni kiriting ")
+// let x2 = +prompt("2-sonni kiriting ")
+
+// if (x1 > 0 || x2 > 0){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 31)Berilgan son juft va 4 ga karrali, lekin 8 ga karrali emasligini aniqlang
+
+// let x = +prompt("sonn kirting")
+
+// if((x % 2 ==0 || x % 4 == 0) && x % 8 != 0){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 33)Berilgan son 2 ga va 5 ga bo'linadi, lekin 10 ga bo'linmaydimi
+
+// let x = +prompt("sonni kiriting")
+
+// if ( x % 2 ==0 && x % 5 == 0 && x % 10 !=0 ){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 34)Ikkita sonning yig'indisi ularning farqidan katta ekanligini aniqlang.
+
+// let x1 = +prompt("1-soni kiriting")
+// let x2 = +prompt("2-soni kiriting")
+
+// if(x1 + x2 > Math.abs( x1 - x2)){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 35)Berilgan son 0 dan katta va 1000 dan kichik ekanligini tekshiring.
+
+// let x = +prompt('sonni kirting ')
+
+// if(x < 1000 && x > 0){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 36)Ikki sonning biri juft va ularning ko'paytmasi 20 dan katta ekanligini aniqlang.
+
+// let x1 = +prompt('1-sonni kirting')
+// let x2 = +prompt('2-sonni kirting')
+
+// if((x1 % 2  ==0 || x2 % 2 ==0) && (x1 *x2 > 20 )){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 37)Uchta sonning hech bo'lmaganda bittasi 10 ga bo'linadi ekanligini tekshiring.
+
+// let x1 = +prompt('1-sonni kirtinng')
+// let x2 = +prompt('2-sonni kirtinng')
+// let x3 = +prompt('3-sonni kirtinng')
+
+// let counter = 0
+// if (x1 % 10 ==0){
+//     counter++
+// }
+// if (x2 % 10 ==0){
+//     counter++
+// }
+// if (x3 % 10 ==0){
+//     counter++
+// }
+
+// console.log(` shuncha son 10 ga bolinadi => ${counter}`);
+
+// 38)Berilgan son 4 ga karrali va toq emasligini aniqlang.
+
+// let x = +prompt('sonni kirting')
+
+// if(x % 4 ==0 && x % 2 != 1){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 39)Uchta sonning ikkitasining yig'indisi uchinchisidan katta ekanligini tekshiring
+
+// let x1 = +prompt('1-sonni kirting')
+// let x2 = +prompt('2-sonni kirting')
+// let x3 = +prompt('3-sonni kirting')
+
+// if( x1 + x2 > x3 || x1 + x3 > x2 || x2 + x3 > x1){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 40) Ikki sonning biri musbat, biri manfiy va ularning ko'paytmasi musbat ekanligini tekshiring.
+
+// let x1 = +prompt('1-sonni kirting')
+// let x2 = +prompt('2-sonni kirting')
+
+// if( ((x1 > 0 && x2 < 0) || (x2> 0 && x1 < 0 )) && (x1 * x2 >0)){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+                                    // QOSHIMCHA MASALALAR
+
+// 1)Uchta sonning yig'indisi manfiy emas va ulardan hech bo'lmaganda bittasi 5 ga bo'linadi.
+
+// let x1 = +prompt('1-sonni kieiting')
+// let x2 = +prompt('2-sonni kieiting')
+// let x3 = +prompt('3-sonni kieiting')
+
+// if(( x1 + x2 + x3 > 0) && (x1 % 5 ==0 || x2 % 5 ==0 || x3 % 5 == 0)){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 3)Ikkita sonning biri 2 ga, biri 3 ga bo'linadi, lekin ularning yig'indisi 6 ga bo'linmaydi
+
+// let x1 = +prompt( "1- sonni kirting")
+// let x2 = +prompt( "2- sonni kirting")
+
+// if((x1 % 2 == 0 && x2 % 3 == 0) || ( x2 % 2 == 0 && x1 % 3 ==0) && ( x1 + x2 )% 6 !=0){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+
+// 4)Uchta sonning hech bo'lmaganda ikkitasining ayirmasi manfiy ekanligini tekshiring
+
+
+// let x1 = +prompt( "1- sonni kirting")
+// let x2= +prompt( "2- sonni kirting")
+// let x3 = +prompt( "3- sonni kirting")
+
+// if( x1 - x2 <0 || x1 - x3 < 0 || x2 - x3 < 0){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// 6) Uchta sonning bittasi musbat, ikkinchisi manfiy, uchinchisi esa 0 ga teng ekanligini tekshiring
+
+// let x1 = +prompt( "1- sonni kirting")
+// let x2= +prompt( "2- sonni kirting")
+// let x3 = +prompt( "3- sonni kirting")
+
+// if( ( x1 > 0 && x2 < 0 x3 ==0) || (x1 > 0 && x3 < 0 && x2 ==0 ) || (x2 >0 && x3 < 0 && x1 == 0)){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
